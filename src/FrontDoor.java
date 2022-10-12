f5adfd63fa1dba6906000df43765396215efc4f3
BT_4N2_18 <jxu5406@bths.edu>
2022-10-12 12:23:22 -0400

    Patchnotes v0.05 Private variables: d px py Removed and replaced by local variables
    Added yValue method, which calculates the location of the y value given an x value on the same line, and returns a string detailing its findings

--- src/FrontDoor.java
+++ src/FrontDoor.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 public class FrontDoor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your first coordinate (x,y)");
+        System.out.print("Please enter your first coordinate (x,y)");
         String coord1 = input.nextLine();
         int extractor1 = coord1.indexOf("(");
         int extractor2 = coord1.indexOf(",");
         int x1 = Integer.parseInt(coord1.substring(extractor1 + 1, extractor1 + 2));
         int y1 = Integer.parseInt(coord1.substring(extractor2 + 1, extractor2 + 2));
         System.out.println("(" + x1 + "," + y1 + ")");
-        System.out.println("Please input your second coordinate (x,y)");
+        System.out.print("Please input your second coordinate (x,y)");
         String coord2 = input.nextLine();
         extractor1 = coord2.indexOf("(");
         extractor2 = coord2.indexOf(",");
         int x2 = Integer.parseInt(coord2.substring(extractor1 + 1, extractor1 + 2));
         int y2 = Integer.parseInt(coord2.substring(extractor2 + 1, extractor2 + 2));
-        LinearEquation test = new LinearEquation(x1,y1,x2,y2);
+        LinearEquation test = new LinearEquation(x1, y1, x2, y2);
+        test.calculateSlope();
+        test.intercept();
+        test.distance();
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,11 +3,8 @@ public class LinearEquation {
     private int x2;
     private int y1;//second pair coordinates
     private int y2;
-    private int px;//placeholder x
-    private int py;//placeholder y
     private int sx;//change in x
     private int sy;//change in y
-    private double d;//distance between 2 points
     private int m;//change in y + "/" + change in x
     public LinearEquation(int x1, int y1, int x2, int y2)
     {
@@ -26,14 +23,20 @@ public class LinearEquation {
     }
     public int intercept()
     {
-        px = x2 / sx;
-        py = y2 - (sy * px);
+        int px = x2 / sx;
+        int py = y2 - (sy * px);
         return py;
     }
     public double distance()
     {
-        d = Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2);
+        double d = Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2);
         d = Math.sqrt(d);
         return d;
+    }
+    public String yValue(int xVal)
+    {
+        double px = (double) xVal / sx; //this holds how many times the change in x was executed
+        double yVal = sy * px; //this uses how many times the change in x was executed and applies it on y to get the y value
+        return "Solved coordinate point is: (" + xVal + "," + yVal + ")";
     }
 }
