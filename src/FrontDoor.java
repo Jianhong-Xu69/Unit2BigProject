94c9275480d22df93015ba0e46a87dacd6ec8738
jianx <jxu5406@bths.edu>
2022-10-13 20:50:31 -0400

    Patchnotes v1.00 Added yValue(), a method that returns the Y value of a given X value on the same object
    Expanded on slopeInterceptForm() to run calculateSlope() and intercept() inside, so it doesn't break when those aren't run first
    
    Expanded on intercept() to run calculateSlope() inside, so it doesn't break when that isn't run first
    
    Expanded on toString() to run the methods inside, so it doesn't break if those aren't run first
    
    Removed private variable m from LinearEquation
    
    Finished adding runner code to FrontDoor
    
    Commented code
    
    Updated DesignDocument with statuses

--- src/FrontDoor.java
+++ src/FrontDoor.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
+import java.util.Scanner;//Scanner my beloved (Allows user input)
 public class FrontDoor {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);//setup for input
         System.out.println("Please enter your first coordinate (x,y)");
         String coord1 = input.nextLine();
-        int extractor1 = coord1.indexOf("(");
+        int extractor1 = coord1.indexOf("(");//extracts the ints from String of first coordinate
         int extractor2 = coord1.indexOf(",");
-        int x1 = Integer.parseInt(coord1.substring(extractor1 + 1, extractor1 + 2));
-        int y1 = Integer.parseInt(coord1.substring(extractor2 + 1, extractor2 + 2));
-        System.out.println("(" + x1 + "," + y1 + ")");
+        int extractor3 = coord1.indexOf(")");
+        int x1 = Integer.parseInt(coord1.substring(extractor1 + 1, extractor2));
+        int y1 = Integer.parseInt(coord1.substring(extractor2 + 1, extractor3));
         System.out.println("Please input your second coordinate (x,y)");
         String coord2 = input.nextLine();
-        extractor1 = coord2.indexOf("(");
+        extractor1 = coord2.indexOf("(");//extracts the ints from String of seconds coordinate
         extractor2 = coord2.indexOf(",");
-        int x2 = Integer.parseInt(coord2.substring(extractor1 + 1, extractor1 + 2));
-        int y2 = Integer.parseInt(coord2.substring(extractor2 + 1, extractor2 + 2));
-        LinearEquation test = new LinearEquation(x1, y1, x2, y2);
-        test.calculateSlope();
-        test.intercept();
-        test.distance();
-        System.out.println(test);
+        extractor3 = coord2.indexOf(")");
+        int x2 = Integer.parseInt(coord2.substring(extractor1 + 1, extractor2));
+        int y2 = Integer.parseInt(coord2.substring(extractor2 + 1, extractor3));
+        LinearEquation test = new LinearEquation(x1, y1, x2, y2);//creates object
+        System.out.println(test);//toString() method allows it to print the value of the object
+        System.out.println("Enter an X integer value on the line to calculate the Y value");
+        System.out.println(test.yValue(input.nextDouble()));
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,19 +1,18 @@
 public class LinearEquation {
     private int x1;//first pair coordinates
-    private int x2;
-    private int y1;//second pair coordinates
+    private int y1;
+    private int x2;//second pair coordinates
     private int y2;
     private int sx;//change in x
     private int sy;//change in y
-    private int m;//change in y + "/" + change in x
-    public LinearEquation(int x1, int y1, int x2, int y2)
+    public LinearEquation(int x1, int y1, int x2, int y2)//constructor
     {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
-    public double calculateSlope()
+    public double calculateSlope()//returns the slope of the object as a double
     {
         int changeY = y2 - y1;
         int changeX = x2 - x1;
@@ -21,34 +20,34 @@ public class LinearEquation {
         sx = changeX;
         return (double)changeY / (double)changeX;
     }
-    public String slopeInterceptForm()
+    public String slopeInterceptForm()//returns the slope and the Y intercept of the object in slope intercept form
     {
         calculateSlope();
-        return "Slope intercept form: y = " + sy + "/" + sx + "+(" + intercept() + ")";
+        return "Slope intercept form: y = (" + sy + "/" + sx + ") + (" + intercept() + ")";
     }
-    public int intercept()
+    public int intercept()//finds and returns the Y intercept of the object
     {
         calculateSlope();
-        int px = x2 / sx;
-        int py = y2 - (sy * px);
-        return py;
+        int cx = x2 / sx;//cx for change in x
+        return y2 - (sy * cx);
     }
-    public double distance()
+    public double distance()//finds and returns the distance between (x1,y1) and (x2,y2)
     {
         double d = Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2);
         d = Math.sqrt(d);
+        d = Math.round(d * 100.00) / 100.00;
         return d;
     }
-    public String yValue(int xVal)
+    public String yValue(double xVal)//returns Y value of a point on a point on the object given an X value
     {
         calculateSlope();
-        double px = (double) xVal / sx; //this holds how many times the change in x was executed
+        double px = xVal / sx; //this holds how many times the change in x was executed
         double yVal = sy * px; //this uses how many times the change in x was executed and applies it on y to get the y value
         return "Solved coordinate point is: (" + xVal + "," + yVal + ")";
     }
-    public String toString()
+    public String toString() //prints the data of the object instead of the memory location
     {
-        String result = "First Pair: (" + x1 + "," + y1 + ")\nSecond Pair: (" + x1 + "," + y1 + ")";
+        String result = "First Pair: (" + x1 + "," + y1 + ")\nSecond Pair: (" + x2 + "," + y2 + ")";
         result += "\nSlope of line: " + calculateSlope();
         result += "\nY intercept: " + intercept();
         result += "\n" + slopeInterceptForm();
